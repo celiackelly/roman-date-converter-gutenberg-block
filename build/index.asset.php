ba4fcb298c312a5db87351fe20547f04fbd36263
Celia Kelly <celiackelly@gmail.com>
2023-04-06 14:27:12 -0400

    Run build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '4f1f79e81110fa443928');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '514d5474863450d15df1');
