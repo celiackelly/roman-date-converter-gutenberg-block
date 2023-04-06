fcd572652e40f599257c12e154641648b852a5d8
Celia Kelly <celiackelly@gmail.com>
2023-04-06 15:04:20 -0400

    Change plugin icon for admin dashboard in block.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '514d5474863450d15df1');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6eb06bb16a7e1230b276');
